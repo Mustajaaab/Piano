5eadc3fa1e41418ff76c34fb6c4682dc66b094a3
Mustajaaab <mustajabnabi@gmail.com>
2025-05-18 17:05:17 +0500

    Link component in the Header is Done and the Privacy Policy Page Also done!

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,11 +9,11 @@ const Header = () => {
             <Image src='/images/player.png' alt="Logo" width={293} height={45} className="cursor-pointer"/>
             <nav>
                 <ul className="flex gap-9">
-                    <li className="text-[#3AB24A] cursor-pointer font-semibold text-sm"><Link href=''>Virtual Piano</Link></li>
+                    <li className="text-[#3AB24A] cursor-pointer font-semibold text-sm"><Link href='/'>Virtual Piano</Link></li>
                     <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href='/tuner'>Tuner</Link></li>
-                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href=''>Basic Lessons</Link></li>
-                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href=''>Privacy Policy</Link></li>
-                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href=''>Contact Us</Link></li>
+                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href='/basic-lessons'>Basic Lessons</Link></li>
+                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href='/privacy-policy'>Privacy Policy</Link></li>
+                    <li className="text-[#494949] cursor-pointer font-semibold text-sm"><Link href='/contact-us'>Contact Us</Link></li>
                 </ul>
             </nav>
         </div>
